d3f9308e4b64944ba017da8f125d88cb9a7edb6e
TavaresDylan <tavares-dylan03@gmail.com>
2019-04-09 09:36:00 +0200

    Ajout Page + structure form

---
+++ Purchase_order.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Purchase Order</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+	<form>
+
+		<label>Prénom :</label>
+		<input type="" name="">
+
+		<label>Nom :</label>
+		<input type="" name="">
+
+		<label>Rue/Voie :</label>
+		<input type="" name="">
+
+		<label>Code postal :</label>
+		<input type="" name="">
+
+		<label>Ville :</label>
+		<input type="" name="">
+
+		<label></label>
+		<input type="" name="">
+
+		<label>N° Tel :</label>
+		<input type="tel" name="">
+
+		<label>Mail :</label>
+		<input type="email" name="mail">
+
+
+	</form>
+
+
+</body>
+</html>
